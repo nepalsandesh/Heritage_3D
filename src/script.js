1213f9c6a1033f2c20d8a2d2cfc5414dbf854fda
nepalsandesh <sandeshnepal43@gmail.com>
2024-02-14 11:38:50 +0545

    Nayatpol Temple added

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Fog
-const fog = new THREE.Fog('#262837', 1, 150)
+const fog = new THREE.Fog('#262837', 1, 300)
 scene.fog = fog  
 
 /**
@@ -31,6 +31,10 @@ const bricksColorTexture = textureLoader.load('/textures/bricks/temple.jpg')
 const bricksAmbientOcclusionTexture = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
 const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
 const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
+const nayatpolTexture = textureLoader.load('/textures/nayatpola/nayatpola3.jpg')
+const nayatpolUpTexture = textureLoader.load('/textures/nayatpola/nayatpolatop.jpg')
+const nayatpolBaseTexture = textureLoader.load('/textures/nayatpola/base.jpg')
+const templeTexture = textureLoader.load('/textures/bricks/temple.jpg')
 
 const tileColorTexture = textureLoader.load('/textures/floor/tile.jpg')
 const tileAmbientOcclusionTexture = textureLoader.load('/textures/floor/alpha.jpg')
@@ -49,12 +53,13 @@ tileAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping
 tileRoughnessTexture.wrapT = THREE.RepeatWrapping
 
 
+
 /**
  * House
  */
 // Group 1
 const house = new THREE.Group()
-scene.add(house)
+// scene.add(house)
 
 // Walls
 const walls = new THREE.Mesh(
@@ -135,8 +140,8 @@ for(let x=-5; x<5; x++){
         durbarRoof.rotation.y = Math.PI * 0.25
         durbar.add(durbarRoof)
         
-        durbar.position.x = x * 20
-        durbar.position.z = z * 20
+        durbar.position.x = x * 30
+        durbar.position.z = z * 30
 
         // Durbar Roof 2
         const durbarRoof2 = new THREE.Mesh(
@@ -149,6 +154,7 @@ for(let x=-5; x<5; x++){
         durbarRoof2.scale.z = 0.8
         durbarRoof2.rotation.y = Math.PI * 0.25
         durbar.add(durbarRoof2)
+        durbar.scale.y =  2 + Math.random() * 5
 
     }
 }
@@ -157,7 +163,7 @@ for(let x=-5; x<5; x++){
 
 // Floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(230, 230),
+    new THREE.PlaneGeometry(400, 400),
     new THREE.MeshStandardMaterial({
         map: tileColorTexture,
         aoMap: tileAmbientOcclusionTexture,
@@ -169,6 +175,116 @@ floor.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(floor.geomet
 floor.rotation.x = - Math.PI * 0.5
 floor.position.y = 0
 scene.add(floor)
+
+
+/**
+ * Nayatpola Temple
+ */
+
+const nayatpolGroup = new THREE.Group()
+scene.add(nayatpolGroup)
+
+const nayatpolBaseGeometry = new THREE.BoxGeometry(10, 1, 10)
+const nayatpolBaseMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map:nayatpolBaseTexture  })
+const nayatpolBase = new THREE.Mesh(nayatpolBaseGeometry, nayatpolBaseMaterial)
+nayatpolBase.position.y = 1
+
+const nayatpolBase2 =  nayatpolBase.clone(true)
+nayatpolBase2.scale.set(0.9, 1, 0.9)
+nayatpolBase2.position.y = 1
+
+const nayatpolBase3 =  nayatpolBase.clone(true)
+nayatpolBase3.scale.set(0.8, 1, 0.8)
+nayatpolBase3.position.y = 2
+
+const nayatpolBase4 =  nayatpolBase.clone(true)
+nayatpolBase4.scale.set(0.7, 1, 0.7)
+nayatpolBase4.position.y = 3
+
+const nayatpolBase5 =  nayatpolBase.clone(true)
+nayatpolBase5.scale.set(0.6, 1, 0.6)
+nayatpolBase5.position.y = 4
+
+nayatpolGroup.add(nayatpolBase, nayatpolBase2, nayatpolBase3, nayatpolBase4, nayatpolBase5)
+
+
+const nayatpolRoomGeometry = new THREE.BoxGeometry(4, 4, 4)
+const nayatpolRoomMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map:nayatpolTexture})
+const nayatpolRoomUpMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map:templeTexture})
+const nayatpolRoom = new THREE.Mesh(nayatpolRoomGeometry, nayatpolRoomUpMaterial)
+const nayatpolRoom2 = new THREE.Mesh(nayatpolRoomGeometry, nayatpolRoomUpMaterial)
+const nayatpolRoom3 = new THREE.Mesh(nayatpolRoomGeometry, nayatpolRoomUpMaterial)
+const nayatpolRoom4 = new THREE.Mesh(nayatpolRoomGeometry, nayatpolRoomUpMaterial)
+const nayatpolRoom5 = new THREE.Mesh(nayatpolRoomGeometry, nayatpolRoomUpMaterial)
+
+nayatpolRoom.position.y = 5
+nayatpolRoom2.position.y = 8
+nayatpolRoom2.scale.set(0.7, 0.7, 0.7)
+
+nayatpolRoom3.position.y = 10
+nayatpolRoom3.scale.set(0.5, 0.5, 0.5)
+
+nayatpolRoom4.position.y = 12
+nayatpolRoom4.scale.set(0.3, 0.3, 0.3)
+
+nayatpolRoom5.position.y = 13.5
+nayatpolRoom5.scale.set(0.1, 0.1, 0.1)
+
+nayatpolGroup.add(nayatpolRoom, nayatpolRoom2, nayatpolRoom3, nayatpolRoom4, nayatpolRoom5)
+
+const nayatpolaRoof = roof.clone()
+nayatpolaRoof.material.color.set(0xffaa66)
+nayatpolaRoof.scale.set(1.3, 2, 1.3)
+nayatpolaRoof.position.y = 8
+
+
+
+const nayatpolaRoof2 = nayatpolaRoof.clone()
+nayatpolaRoof2.scale.set(1,  1.5, 1)
+nayatpolaRoof2.position.y = 10
+
+const nayatpolaRoof3 = nayatpolaRoof.clone()
+nayatpolaRoof3.scale.set(0.7,  1.2, 0.7)
+nayatpolaRoof3.position.y = 11.5
+
+const nayatpolaRoof4 = nayatpolaRoof.clone()
+nayatpolaRoof4.scale.set(0.5,  1, 0.5)
+nayatpolaRoof4.position.y = 13
+
+const nayatpolaRoof5 = nayatpolaRoof.clone()
+nayatpolaRoof5.scale.set(0.2,  0.3, 0.2)
+nayatpolaRoof5.position.y = 13.8
+
+nayatpolGroup.add(nayatpolaRoof, nayatpolaRoof2, nayatpolaRoof3, nayatpolaRoof4, nayatpolaRoof5 )
+nayatpolGroup.scale.set(10, 10, 10)
+nayatpolGroup.position.y = -3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Dharahara
+ */
+
+
+
+
+/**Sajha Yatayat */
+
+
+
+
 
 /**
  * Lights
@@ -222,10 +338,10 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.001, 1000)
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.001, 1000)
 camera.position.x = 4
 camera.position.y = 4
-camera.position.z = 4
+camera.position.z = 150
 scene.add(camera)
 
 // // Controls
@@ -237,7 +353,7 @@ scene.add(camera)
 const pControls = new PointerLockControls(camera, canvas)
 const playButton = document.getElementById('menuPanel')
 playButton.onclick = () =>
-{
+{   
     pControls.lock()
     playButton.style.display = 'none'
 }
@@ -307,6 +423,12 @@ const tick = () =>
     }
     if (keyMap['KeyD'] || keyMap['ArrowRight']) {
         pControls.moveRight(0.25)
+    }
+    if (keyMap['KeyE']) {
+        camera.position.y += 0.1
+    }
+    if (keyMap['KeyQ']) {
+        camera.position.y -= 0.1
     }
 
 
